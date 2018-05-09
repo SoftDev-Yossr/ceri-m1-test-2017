99cc29758f6ccb4249a5ea76a74624cc74e49295
Mufti Yossr <uapv1800833@ecv-2w26-105.univ-avignon.fr>
2018-05-09 15:05:26 +0200

    Animal commit

--- src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
@@ -1,30 +1,50 @@
 package fr.univavignon.rodeo.api;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.mockito.Mockito;
 
 
 public class IAnimalTest{
 
+	public static IAnimal createAnimal(int getXP, boolean isSecret, boolean isEndangered, boolean isBoss) {
+		IAnimal animalMock = Mockito.mock(IAnimal.class);
+		Mockito.when(animalMock.getXP()).thenReturn(getXP);
+		Mockito.when(animalMock.isSecret()).thenReturn(isSecret);
+		Mockito.when(animalMock.isEndangered()).thenReturn(isEndangered);
+		Mockito.when(animalMock.isBoss()).thenReturn(isBoss);
+		return animalMock;
+}
+	
+	protected IAnimal animalMok(){
+		return createAnimal(2,true,false,false);
+		
+	}
+	
 	@Test
 public void getXP() {
+		assertEquals(2,animalMok().getXP());
 	
 
 }
   @Test
 public void isBossTest(){
 	
-	
+		assertEquals(false,animalMok().isBoss());
+
 }
   @Test
 public void isEndangeredTest(){
 	
-		
+	  assertEquals(false,animalMok().isEndangered());
 }
   @Test
 public void isSecretTest(){
 	
 	
-	
+	  assertEquals(true,animalMok().isEndangered());
+
 }
 
 }
