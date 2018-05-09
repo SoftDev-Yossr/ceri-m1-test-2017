a4fafaab291b8b7ca7df64c8e6d85b50f34b0408
Mufti Yossr <uapv1800833@ecv-2w26-105.univ-avignon.fr>
2018-05-09 15:10:05 +0200

    Animal commit

--- src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
@@ -8,42 +8,43 @@ import org.mockito.Mockito;
 
 public class IAnimalTest{
 
+	//Mock creation  
 	public static IAnimal createAnimal(int getXP, boolean isSecret, boolean isEndangered, boolean isBoss) {
-		IAnimal animalMock = Mockito.mock(IAnimal.class);
-		Mockito.when(animalMock.getXP()).thenReturn(getXP);
-		Mockito.when(animalMock.isSecret()).thenReturn(isSecret);
-		Mockito.when(animalMock.isEndangered()).thenReturn(isEndangered);
-		Mockito.when(animalMock.isBoss()).thenReturn(isBoss);
-		return animalMock;
+		IAnimal animal = Mockito.mock(IAnimal.class);
+		Mockito.when(animal.getXP()).thenReturn(getXP);
+		Mockito.when(animal.isSecret()).thenReturn(isSecret);
+		Mockito.when(animal.isEndangered()).thenReturn(isEndangered);
+		Mockito.when(animal.isBoss()).thenReturn(isBoss);
+		return animal;
 }
 	
-	protected IAnimal animalMok(){
+	protected IAnimal animal(){
 		return createAnimal(2,true,false,false);
 		
 	}
 	
 	@Test
 public void getXP() {
-		assertEquals(2,animalMok().getXP());
+		assertEquals(2,animal().getXP());
 	
 
 }
   @Test
 public void isBossTest(){
 	
-		assertEquals(false,animalMok().isBoss());
+		assertEquals(false,animal().isBoss());
 
 }
   @Test
 public void isEndangeredTest(){
 	
-	  assertEquals(false,animalMok().isEndangered());
+	  assertEquals(false,animal().isEndangered());
 }
   @Test
 public void isSecretTest(){
 	
 	
-	  assertEquals(true,animalMok().isEndangered());
+	  assertEquals(true,animal().isEndangered());
 
 }
 
